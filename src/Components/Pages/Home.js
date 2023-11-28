6c8321f6c57a9ceffbf3f23dbeaef9a35811b017
Ahmad Ali khan <ahmadalikhan6534@gmail.com>
2023-11-28 19:57:03 +0500

    update

---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../Navbar'
+import Hero from '../Hero'
+import ProductCard from '../ProductCard'
+import Footer from '../Footer'
+
+const Home = () => {
+  return (
+    <div>
+     
+        <Hero/>
+        <ProductCard/>
+      
+    </div>
+  )
+}
+
+export default Home

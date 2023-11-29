0a5a790e97e15ed895c8cfc708c6f699702b516c
Ahmad Ali khan <ahmadalikhan6534@gmail.com>
2023-11-29 20:11:04 +0500

    sadasdasd

--- src/index.js
+++ src/index.js
@@ -10,10 +10,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+<Provider store={Store}>
 <BrowserRouter>
 
+<React.StrictMode>
+    <App />
+  </React.StrictMode>
+
 </BrowserRouter>
+</Provider>
 
 );
 

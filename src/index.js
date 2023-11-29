b308d83773e77c134383d295453fa698cc5e70a6
Malik-Sohaib <maliksohaib229@gmail.com>
2023-11-29 20:07:49 +0500

    update

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import reportWebVitals from './reportWebVitals';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter } from 'react-router-dom';
+import { Store } from './App/Store';
+import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+<Provider store={Store}>
 <BrowserRouter>
 <React.StrictMode>
     <App />
   </React.StrictMode>
 </BrowserRouter>
+</Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
